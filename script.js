4bbd6182930b26f77d72cd8486f5026f1be88f6f
Oliver Hogan <oliver@oliver-hogan.com>
2024-02-04 13:47:27 +1100

    added x divs

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ function spawnSquares(size) {
     for (let yIndex = 0; yIndex < size; yIndex++) {
         //Create Y divs
         const yDiv = document.createElement("div");
+
         yDiv.className = "yDiv";
         yDiv.id = yIndex.toString();
         container.appendChild(yDiv);
 
         //Create X divs
+        for (let xIndex = 0; xIndex < size; xIndex++) {
+            const xDiv = document.createElement("div");
 
+            xDiv.className = "xDiv";
+            xDiv.id = xIndex.toString();
+            yDiv.appendChild(xDiv);
+        }
     }
 
 }
