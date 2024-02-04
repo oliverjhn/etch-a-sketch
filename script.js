cc54df5d6883160b374ad852f09a0c134db4affc
Oliver Hogan <oliver@oliver-hogan.com>
2024-02-04 14:24:07 +1100

    fixed borders

--- script.js
+++ script.js
@@ -9,17 +9,27 @@ function spawnSquares(size) {
         yDiv.id = yIndex.toString();
         container.appendChild(yDiv);
 
+        if (yIndex === size - 1) {
+            yDiv.style.cssText = "border-bottom: 0.5px solid";
+        }
+        
         //Create X divs
         for (let xIndex = 0; xIndex < size; xIndex++) {
             const xDiv = document.createElement("div");
 
             xDiv.className = "drawDiv xDiv";
             xDiv.id = xIndex.toString();
-            xDiv.textContent = "X";
+            // if (xIndex === size - 1) {
+            //     xDiv.style.cssText = "border-right: 0.5px solid";
+            // }
             yDiv.appendChild(xDiv);
         }
     }
 
 }
 
-spawnSquares(2);
+function colorSquare(params) {
+
+}
+
+spawnSquares(16);
