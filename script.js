ab2c546044ea6063955b8fd899042d6046285319
Oliver Hogan <oliver@oliver-hogan.com>
2024-02-11 13:55:02 +1100

    working on ranodm coluor

--- script.js
+++ script.js
@@ -35,9 +35,48 @@ function spawnSquares(size) {
 
 }
 
+// function hexToRgbA(hex) {
+//     var c;
+//     if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+//         c = hex.substring(1).split('');
+//         if (c.length == 3) {
+//             c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+//         }
+//         c = '0x' + c.join('');
+//         return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',1)';
+//     }
+//     throw new Error('Bad Hex');
+// }
+
+function hexToRgbA(hex, alpha) {
+    var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha) {
+        return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    } else {
+        return "rgb(" + r + ", " + g + ", " + b + ")";
+    }
+}
+
 function colorSquare(event) {
     let target = event.target;
-    target.style.backgroundColor = "black";
+    if (!target.classList.contains("hasColor")) {
+        target.classList.add("hasColor");
+        let currentColor = target.style.backgroundColor;
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+        target.style.backgroundColor = hexToRgbA("#" + randomColor);
+
+        let rgbaValues = currentColor.match(/\d+/g);
+        // rgbaValues[2] = 0.1;
+
+    }
+    // console.log(target.style.opacity);
+    // console.log(target.style.opacity + 0.1)
+    // target.style.opacity = target.style.opacity + 0.1;
+    // console.log(target.style.opacity);
 }
 
 numberInput.addEventListener("input", (e) => {
